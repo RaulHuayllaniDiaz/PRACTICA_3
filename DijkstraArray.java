909bd6f3f84b361720954611ebc9ddf3482a7cc1
Raul Huayllani Diaz <68405601+RaulHuayllaniDiaz@users.noreply.github.com>
2022-03-18 20:50:48 -0500

    Update DijkstraArray.java

--- DijkstraArray.java
+++ DijkstraArray.java
@@ -1,4 +1,45 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
+public class DijkstraArray {
+
+	static ArrayList<nodeList> nodeList = new ArrayList<nodeList>();
+
+	public DijkstraArray(nodeList[] nodeList) {
+		for (nodeList v : nodes) {
+			nodeList.add(v);}
+	}
+
+	static double[] distance = null;
+
+	public double[] Dijkstra(nodeList source) {
+		distance = new double[nodeList.size()];
+		for (int i = 0; i < distance.length; i++) {
+			distance[i] = Double.POSITIVE_INFINITY;}
+
+		distance[nodeList.indexOf(source)] = 0;
+
+		PriorityQueue<nodeList> queue = new PriorityQueue<nodeList>();
+		queue.add(source);
+
+		while (!queue.isEmpty()) {
+			nodeList a = queue.poll();
+			for (Edge e : u.adj) {
+
+				nodeList v = e.finish;
+				double weight = e.weight;
+				double dist = distance[nodeList.indexOf(a)] + weight;
+				if (dist < distance[nodeList.indexOf(v)]) {
+					queue.remove(v);
+					distance[nodeList.indexOf(v)] = dist;
+					v.parent = a;
+					queue.add(v);
+				}
+			}
+		}
+		return distance;
+	}
+}
 //class arista
 class Edge {
 	public final nodeList finish;
