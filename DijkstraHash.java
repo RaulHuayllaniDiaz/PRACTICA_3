ae3bc81272065debd7c6208e74d8b48ca81209cc
Raul Huayllani Diaz <68405601+RaulHuayllaniDiaz@users.noreply.github.com>
2022-03-18 20:15:44 -0500

    Update DijkstraHash.java

--- DijkstraHash.java
+++ DijkstraHash.java
@@ -1,1 +1,13 @@
-ewt
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.HashMap;
+
+public class DijkstraHash {
+	ArrayList<Node> nodeList = new ArrayList<Node>();
+    //atributos
+	public DijkstraHash (Node[] nodes) {
+		for (Node v : nodes) {
+			listNodes.add(v);}
+	}
+
+	
