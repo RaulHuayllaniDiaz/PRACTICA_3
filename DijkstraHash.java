ac45bbe1f6d6bef82b3169ab02a056fa3ec22d8f
Raul Huayllani Diaz <68405601+RaulHuayllaniDiaz@users.noreply.github.com>
2022-03-18 20:31:26 -0500

    Update DijkstraHash.java

--- DijkstraHash.java
+++ DijkstraHash.java
@@ -3,11 +3,36 @@ import java.util.PriorityQueue;
 import java.util.HashMap;
 
 public class DijkstraHash {
-	ArrayList<Node> nodeList = new ArrayList<Node>();
+	ArrayList<nodeList> nodeList = new ArrayList<nodeList>();
     //atributos
-	public DijkstraHash (Node[] nodes) {
-		for (Node v : nodes) {
-			listNodes.add(v);}
+	public DijkstraHash (nodeList[] nodes) {
+		for (nodeList v : nodes) {
+			nodeList.add(v);}
+	}
+	static HashMap<nodeList, Double> distance = new HashMap<nodeList, Double>();
+
+	public HashMap<nodeList, Double> Dijkstra(nodeList source) {
+		for (nodeList node2 : nodeList) {
+			distance.put(node2, Double.POSITIVE_INFINITY);
+		}
+		distance.put(source, 0.0);
+		PriorityQueue<nodeList> queue = new PriorityQueue<nodeList>();
+		queue.add(source);
+		while (!queue.isEmpty()) {
+			nodeList a = queue.poll();
+			for (Edge e : a.adj) {
+				nodeList v = e.finish;
+				double weight = e.weight;
+				double dist = distance.get(a) + weight;
+				if (dist < distance.get(v)) {
+					queue.remove(v);
+					distance.put(v, dist);
+					v.parent = a;
+					queue.add(v);
+				}
+			}
+		}
+		return distance;
 	}
 }
 //class arista
