07a7be4fc01e52848e4754ae0758af3ad2c930d8
Raul Huayllani Diaz <68405601+RaulHuayllaniDiaz@users.noreply.github.com>
2022-03-18 20:23:25 -0500

    Update DijkstraHash.java

--- DijkstraHash.java
+++ DijkstraHash.java
@@ -9,5 +9,40 @@ public class DijkstraHash {
 		for (Node v : nodes) {
 			listNodes.add(v);}
 	}
+}
+//class arista
+class Edge {
+	public final nodeList finish;
+	public final double weight;
+
+	public Edge(Node2 finish, double weight) {
+		finish = this.finish;
+		weight = this.weight;
+	}
+	public Edge(nodeList node, int weight){
+		finish = this.node;
+		weight = this.weight;
+	}
+}
+    
+//class lista de vertices
+class nodeList implements Comparable<nodeList> {
+
+	public final String value;
+        public nodeList parent;
+	public Edge[] adj;
+
+	public nodeList(String val) {
+		value = val;
+	}
+
+	public String toString() {
+		return value;
+	}
+        
+	public int compareTo(nodeList comp) {
+		return Double.compare(DijkstraHash.distance.get(this), DijkstraHash.distance.get(comp));
+	}
+}
 
 	
