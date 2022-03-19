99619880c429b63333d435f73b903b982e413bc2
Raul Huayllani Diaz <68405601+RaulHuayllaniDiaz@users.noreply.github.com>
2022-03-18 20:42:52 -0500

    Update DijkstraArray.java

--- DijkstraArray.java
+++ DijkstraArray.java
@@ -1,1 +1,31 @@
-eekfujb
+
+//class arista
+class Edge {
+	public final nodeList finish;
+	public final double weight;
+
+	public Edge(nodeList finish, double weight) {
+		finish = this.finish;
+		weight = this.weight;
+	}
+}
+    
+//class lista de vertices
+class nodeList implements Comparable<nodeList> {
+
+	public final String value;
+  public nodeList parent;
+	public Edge[] adj;
+
+	public nodeList(String val) {
+		value = val;
+	}
+
+	public String toString() {
+		return value;
+	}
+        
+	public int compareTo(nodeList comp) {
+		return Double.compare(DijkstraArray.distance[DijkstraArray.nodeList.indexOf(this)], DijkstraArray.distance[DijkstraArray.nodeList.indexOf(comp)]);
+	}
+}
